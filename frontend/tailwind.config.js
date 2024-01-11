bbd4e4013c0ffb98953b76c27aaba1f1630b7f53
ArnavRupde <arnavrupade@gmail.com>
2024-01-11 23:13:11 +0530

    Add Tailwind and Create UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    textColor: {
+      white: "#FFF",
+      black: "#000",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
